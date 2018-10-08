ad6a2b9e09b3051885a25683f39cc4ecfeaedbdf
Peio Murguia <peiomurguia@gmail.com>
2018-10-08 14:14:56 +0200

    Inicio

---
+++ Ejercicio 2-2/Ejercicio 2-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_2
+{
+    class Program
+    {
+        public static 
+        static void Main(string[] args)
+        {
+            //Crea una aplicación que nos calcule el área de un circulo,
+            //cuadrado o triangulo.Pediremos que figura queremos calcular su área
+            //y según lo introducido pedirá los valores necesarios para calcular el área.
+            //Crea un método por cada figura para calcular cada área, este devolverá un número real. 
+            //Muestra el resultado por pantalla
+            //Aquí te mostramos que necesita cada figura:
+            //    Circulo: (radio ^ 2) * PI
+            //    Triangulo: (base * altura) / 2
+            //    Cuadrado: lado* lado
+
+        }
+    }
+}
