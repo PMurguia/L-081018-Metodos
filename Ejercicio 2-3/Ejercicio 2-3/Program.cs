e95e7f89c815ee5a3a72772813c4b748daba5b1a
Peio Murguia <peiomurguia@gmail.com>
2018-10-08 16:34:49 +0200

    Subida

---
+++ Ejercicio 2-3/Ejercicio 2-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea una aplicación que nos genere una cantidad de números enteros aleatorios que nosotros le pasaremos por teclado.
+            //Crea un método donde pasamos como parámetros entre que números queremos que los genere,
+            //podemos pedirlas por teclado antes de generar los números. Este método devolverá un número entero aleatorio.
+            //Muestra estos números por pantalla.
+
+            Console.WriteLine("Generemos números aleatorios. Dame un número. ");
+            int cantidad = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Dame un número para usarlo como base (el mínimo).");
+            int numMin = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ahora el tope (máximo).");
+            int numMax = Int32.Parse(Console.ReadLine());
+
+            bool  
+
+        }
+    }
+}
