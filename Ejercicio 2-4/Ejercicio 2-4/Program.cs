dffa3b3213bb51535e1f475d6d84b632cc70a49a
Peio Murguia <peiomurguia@gmail.com>
2018-10-09 16:44:55 +0200

    Subida

---
+++ Ejercicio 2-4/Ejercicio 2-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea una aplicación que nos calcule el factorial de un número pedido por teclado,
+            //lo realizara mediante un método al que le pasamos el número como parámetro. Para calcular el factorial,
+            //se multiplica los números anteriores hasta llegar a uno. Por ejemplo, si introducimos un 5,
+            //realizara esta operación 5 * 4 * 3 * 2 * 1 = 120.
+
+            Console.WriteLine("Saquemos ahora el factorial de un número. ");
+            int factor = Int32.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
