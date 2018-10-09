d7def35238205046e337790857bc23116e0f2a7a
Peio Murguia <peiomurguia@gmail.com>
2018-10-09 10:20:33 +0200

    Subida

---
+++ Parejas Pedir Día/Parejas Pedir Día/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parejas_Pedir_Día
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un día. ");
+            int dia = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce un mes. ");
+            int mes = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce un año. ");
+            int anio = Int32.Parse(Console.ReadLine());
+        }
+    }
+}
