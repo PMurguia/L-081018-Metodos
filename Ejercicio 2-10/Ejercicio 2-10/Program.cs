8a70b3094f37fe752663f7d08ce872311d8e1997
Peio Murguia <peiomurguia@gmail.com>
2018-10-09 10:10:25 +0200

    Subida

--- Ejercicio 2-10/Ejercicio 2-10/Program.cs
+++ Ejercicio 2-10/Ejercicio 2-10/Program.cs
@@ -8,7 +8,7 @@ namespace Ejercicio_2_10
 {
     class Program
     {
-     
+
 
         static void Main(string[] args)
         {
@@ -35,27 +35,38 @@ namespace Ejercicio_2_10
             Console.WriteLine(PedirMes(mes));
             Console.ReadLine();
 
-            
+
             Console.WriteLine("Dame un año. ");
             int num1 = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Dame otro. ");
             int num2 = Int32.Parse(Console.ReadLine());
             Console.WriteLine();
-            Console.WriteLine(PedirAnio(num1,num2));
+            Console.WriteLine(PedirAnio(num1, num2));
             Console.ReadLine();
+
+            Console.WriteLine("Elige un día. ");
+            int dia = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine(PedirDia(mes,dia));
+            Console.ReadLine();
+
+           
+
+
+
         }
         public static int PedirMes(int mes)
         {
-           while(mes<=0 || mes>12)
+            while (mes <= 0 || mes > 12)
             {
                 Console.WriteLine("No es un mes correcto. Por favor, introduzca un número de mes correcto. ");
                 mes = Int32.Parse(Console.ReadLine());
             }
-                return mes;
+            return mes;
         }
         public static int PedirAnio(int num1, int num2)
         {
-            int num3=0;
+            int num3 = 0;
             int anioRan = 0;
 
             if (num1 > num2)
@@ -69,15 +80,86 @@ namespace Ejercicio_2_10
             else
             {
                 Random anioRandom = new Random();
-                anioRan = anioRandom.Next(num1, num2+1);
+                anioRan = anioRandom.Next(num1, num2 + 1);
             }
             return anioRan;
         }
-        public static int PedirDia(int dia)
+
+        public static int PedirDia(int anioRan, int mes)
         {
-            
+            int diasMax;
+
+            if (DateTime.IsLeapYear(anioRan) && mes == 2)
+            {
+                diasMax = 29;
+            }
+            else if (!DateTime.IsLeapYear(anioRan) && mes == 2)
+            {
+                diasMax = 28;
+            }
+            else if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
+            {
+                diasMax = 30;
+            }
+            else
+            {
+                diasMax = 31;
+            }
+
+            return diasMax;
         }
 
+        public static int MostrarSiguienteDia1(int dia, int diasMax, int mes, int anio)
+        {
+            if (dia + 1 > diasMax)
+            {
+                dia = 1;
+                mes++;
+            }
+            else if (mes > 12)
+            {
+                anio++;
+            }
+            else
+            {
+                //Console.WriteLine("Error!!");
+            }
+            return dia;
 
+        }
+        public static int MostrarSiguienteMes(int dia, int diasMax, int mes, int anio)
+        {
+            if (dia + 1 > diasMax)
+            {
+                dia = 1;
+                mes++;
+            }
+            else if (mes > 12)
+            {
+                anio++;
+            }
+            else
+            {
+                //Console.WriteLine("Error!!");
+            }
+            return mes;
+        }
+        public static int MostrarSiguienteAnio(int dia, int diasMax, int mes, int anio)
+        {
+            if (dia + 1 > diasMax)
+            {
+                dia = 1;
+                mes++;
+            }
+            else if (mes > 12)
+            {
+                anio++;
+            }
+            else
+            {
+                //Console.WriteLine("Error!!");
+            }
+            return anio;
+        }
     }
 }
